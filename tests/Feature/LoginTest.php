0f66761b2bcfb62a28e3738631747ca9e67e72d9
bali.appkey <bali.appkey@gmail.com>
2022-06-15 12:22:11 +0800

    Update test case

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -9,46 +9,59 @@ use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
+    // Refresh the database before each test
     use RefreshDatabase;
     
     public function test_redirect_root_to_login_screen()
     {
+        // Visit the root page
         $response = $this->get('/');
 
+        // Assert that the response is a redirect to login page
         $response->assertRedirect('/login');
     }
 
     public function test_login_screen_can_be_rendered()
     {
+        // Visit the login page
         $response = $this->get('/login');
 
+        // Assert that the response is successful
         $response->assertStatus(200);
     }
 
     public function test_users_can_authenticate_using_the_login_screen()
     {
+        // Create an admin user
         $user = $this->createAdmin();
+
+        // Proccess the login request
         $response = $this->post('/login', [
             'email' => $user->email,
             'password' => 'password',
         ]);
 
+        // Assert that the user is logged in
         $this->assertAuthenticated();
+        // Assert that the user is redirected to the dashboard
         $response->assertRedirect('/dashboard');
     }
 
     public function test_users_failed_to_authenticate_using_wrong_credentials()
     {
-        $this->assertInvalidCredentials([
-            'email' => 'wrongemail@demo.com',
-            'password' => 'wrongpass',
-        ]);
-
+        // Proccess the login request with wrong credentials
+        
         $response = $this->post('/login', [
             'email' => 'wrongemail@demo.com',
             'password' => 'wrongpass',
         ]);
 
+        $this->assertInvalidCredentials([
+            'email' => 'wrongemail@demo.com',
+            'password' => 'wrongpass',
+        ]);
+
         $response->assertRedirect('/');
+        $response->assertSessionHasErrors('email');
     }
 }
--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -14,20 +14,23 @@ class ProfileTest extends TestCase
 
     public function setUp(): void
     {
+        // Run all the setUp() code defined in parent (TestCase) class
         parent::setUp();
 
+        // Create admin account and login (actingAs)
         $this->actingAs($this->createAdmin());
     }
 
     public function test_profile_screen_can_be_rendered()
     {
+        // Visit the profile page
         $response = $this->get('/setting/profile');
-
         $response->assertStatus(200);
     }
 
     public function test_users_failed_to_update_profile()
     {
+        // Send wrong request to update the profile
         $response = $this->patch('/setting/profile/update', [
             'name' => '',
             'email' => '',
@@ -35,7 +38,10 @@ class ProfileTest extends TestCase
             'password_confirmation' => '',
         ]);
 
+        // Assert that redirect
         $response->assertStatus(302);
+
+        // Assert if session has error message
         $response->assertSessionHasErrors('name');
         $response->assertSessionHasErrors('email');
         $response->assertSessionHasErrors('password');
@@ -48,9 +54,11 @@ class ProfileTest extends TestCase
             'email' => 'newemail@demo.com',
         ]);
 
+        // Find the user and assert that the name and email are updated
         $user = User::find(Auth::id());
+
         $response->assertRedirect(route('setting.profile.index'));
-        $response->assertSessionHasNoErrors();
+        $response->assertSessionHasNoErrors(); // Assert that session has no error message
         $this->assertEquals('New Name', $user->name);
         $this->assertEquals('newemail@demo.com', $user->email);
     }
@@ -64,10 +72,11 @@ class ProfileTest extends TestCase
             'password_confirmation' => 'newpassword',
         ]);
 
+        // Find the user and assert that the name, email, and password are updated
         $user = User::find(Auth::id());
 
         $response->assertRedirect(route('setting.profile.index'));
-        $response->assertSessionHasNoErrors();
+        $response->assertSessionHasNoErrors(); // Assert that session has no error message
         $this->assertEquals('New Name', $user->name);
         $this->assertEquals('newemail@demo.com' ,$user->email);
         $this->assertTrue(password_verify('newpassword', $user->password));
